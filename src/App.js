474545a13c4c3270849a9d3269eb1679569c86c2
Lindsay Mecher <lindsaymecher@gmail.com>
2020-05-27 17:06:24 -0500

    added basic redux functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import ToDoContainer from './components/ToDoContainer';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch();
+
+  console.log(counter)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ta-Da!</h1>
+      <h3>Accomplishing so many tasks you feel like a wizard.</h3>
+      <h3>You currently have {counter} Ta-Das.</h3>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+      <h3>You are currently {isLogged ? "logged in." : "not logged in."}</h3>
+      <ToDoContainer/>
     </div>
   );
 }
